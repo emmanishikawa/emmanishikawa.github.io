3cbf1faa1eec66bb621a90cfe171abcea4cfdd6b
emmanishikawa <emmanishikawa@gmail.com>
2025-07-19 11:49:08 -0700

    Delete emmanishikawa/emmanishikawa directory

--- emmanishikawa/emmanishikawa/src/components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-export default function NavBar(){
-    return (
-        <>
-            <div className="hidden lg:flex fixed w-screen h-[60px] bg-accent border-b-[1.5px] border-blue-border z-100">
-            
-            </div>
-        </>
-    )
-}
