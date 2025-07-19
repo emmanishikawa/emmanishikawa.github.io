e1dcfa5dd8012c211ca41624dfb8890ce98348e0
emmanishikawa <emmanishikawa@gmail.com>
2025-07-19 16:07:02 -0700

    nav bar mobile layout adjustment

--- emmanishikawa/src/components/NavBar.tsx
+++ emmanishikawa/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ export default function NavBar(){
     return (
         <>
             <div className="flex fixed justify-center w-screen h-[60px] bg-accent/25 border-b-[1.5px] border-blue-border backdrop-blur-md z-100">
-                <div className="flex relative h-auto w-screen lg:w-3xl mx-8 lg:m-0 text-white font-[kodomo] text-[22px] items-center">
+                <div className="flex relative h-auto w-screen lg:w-3xl mx-8 lg:m-0 text-white font-[kodomo] text-[20px] md:text-[22px] items-center">
                     <div>Emma Nishikawa</div>
                     <div className="ml-auto"><NavButton/></div>
                 </div>
