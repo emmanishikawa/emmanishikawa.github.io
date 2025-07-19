56df4aa3b5eaf12a6bad7085aae2150c44ab630b
emmanishikawa <emmanishikawa@gmail.com>
2025-07-19 15:13:53 -0700

    fix nav button layout

--- emmanishikawa/src/components/NavBar.tsx
+++ emmanishikawa/src/components/NavBar.tsx
@@ -5,8 +5,8 @@ export default function NavBar(){
         <>
             <div className="flex fixed justify-center w-screen h-[60px] bg-accent/25 border-b-[1.5px] border-blue-border backdrop-blur-md z-100">
                 <div className="flex relative h-auto w-screen lg:w-3xl mx-8 lg:m-0 text-white font-[kodomo] text-[22px] items-center">
-                    Emma Nishikawa
-                    <NavButton/>
+                    <div>Emma Nishikawa</div>
+                    <div className="ml-auto"><NavButton/></div>
                 </div>
             </div>
         </>
