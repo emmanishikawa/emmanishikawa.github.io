d1dff92bee361df6f8682efac6baf3f97b215b99
emmanishikawa <emmanishikawa@gmail.com>
2025-07-19 16:14:50 -0700

    configuration for projects section

---
+++ emmanishikawa/src/sections/Projects.tsx
@@ -0,0 +1,9 @@
+export default function Projects(){
+    return (
+        <>
+            <div className="flex flex-col w-full h-fit pb-10 text-mygray-500 text-[16px] font-[nunito] border-b-[1.5px] border-blue-border">
+                <h1 className="font-[kodomo] text-white text-3xl my-8">projects</h1>
+            </div>
+        </>
+    )
+}
