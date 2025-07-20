13e0fd6273d1c227edd301935ed9f0d484e43035
emmanishikawa <emmanishikawa@gmail.com>
2025-07-20 12:21:15 -0700

    update content info and added favicon

--- emmanishikawa/src/sections/Footer.tsx
+++ emmanishikawa/src/sections/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return (
         <>
             <div className="flex flex-col w-full h-fit py-2 text-mygray-500 text-[16px] font-[nunito]">
-                developed by Emma Nishikawa
+                developed by Emma Nishikawa 2025
             </div>
         </>
     )
--- emmanishikawa/src/sections/Projects.tsx
+++ emmanishikawa/src/sections/Projects.tsx
@@ -6,7 +6,7 @@ export default function Projects(){
         <>
             <div className="flex flex-col w-full h-fit pb-10 text-mygray-500 text-[16px] font-[nunito] border-b-[1.5px] border-blue-border">
                 <h1 className="font-[kodomo] text-white text-3xl my-8">projects</h1>
-                *projects are still work in progress
+                *projects are still work in progress; view github for more info
                 <div className="flex flex-col md:flex-row justify-center items-center">
                     {projects.map((_project, _index) => (
                         <ProjectBlock index={_index} />
