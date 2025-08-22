45ba82f4382221695dcf589aec14d25b2d2595ca
emmanishikawa <emmanishikawa@gmail.com>
2025-08-22 09:35:23 -0700

    updated footer

--- emmanishikawa/src/sections/Footer.tsx
+++ emmanishikawa/src/sections/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer(){
     return (
         <>
-            <div className="flex flex-col w-full h-fit py-2 text-mygray-500 text-[16px] font-[nunito]">
-                developed by Emma Nishikawa 2025
+            <div className="flex flex-col w-full h-fit pt-2 text-mygray-500 text-[12px] font-[nunito]">
+                developed by Emma Nishikawa.
             </div>
         </>
     )
