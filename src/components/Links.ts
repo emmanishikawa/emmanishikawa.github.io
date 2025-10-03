ce2acc481ae584416771d6567c7c5e57f24d6429
emmanishikawa <emmanishikawa@gmail.com>
2025-10-03 16:49:55 -0700

    update urls

--- src/components/Links.ts
+++ src/components/Links.ts
@@ -2,16 +2,16 @@ export default [
     {
         name: "mail",
         url: "mailto:enishikawa@ucsd.edu",
-        svg_url: "/emmanishikawa/icons/mail.svg",
+        svg_url: "/icons/mail.svg",
     },
     {
         name: "linkden",
         url: "https://www.linkedin.com/in/emmanishikawa/",
-        svg_url: "/emmanishikawa/icons/linkedin.svg",
+        svg_url: "/icons/linkedin.svg",
     },
     {
         name: "github",
         url: "https://github.com/emmanishikawa",
-        svg_url: "/emmanishikawa/icons/github.svg",
+        svg_url: "/icons/github.svg",
     },
 ];
