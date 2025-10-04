1f8741d67a659832b270ce01ef20c343820bc209
emmanishikawa <emmanishikawa@gmail.com>
2025-10-03 19:19:51 -0700

    fix project pg footers, fix cursor

--- src/sections/CaseStudy.tsx
+++ src/sections/CaseStudy.tsx
@@ -3,6 +3,7 @@ import link_svg from "../assets/icons/arrow-up-right.svg";
 import { useParams } from "react-router-dom";
 import projects, { type Project } from "../components/Projects";
 import BlockRenderer from "../components/caseStudyBlocks/BlockRenderer";
+import Footer from "./Footer";
 
 export default function CaseStudy() {
   const { slug } = useParams();
@@ -32,8 +33,10 @@ export default function CaseStudy() {
 
 
   return (
-    <div className="flex h-screen w-screen justify-center ">
-      <div className="flex flex-col relative w-screen h-full mx-8 lg:m-0 lg:w-3xl top-14 lg:top-25 ">     
+    <>
+    <div className="flex w-screen justify-center ">
+      <div className="flex flex-col relative w-screen mx-8 lg:m-0 lg:w-3xl top-14 lg:top-25 ">     
+      <div className="border-b-[1.5px] border-blue-border">
       <div className="flex flex-row gap-2">  
       <h1 className="font-[kodomo] text-white text-3xl my-8">
         {project.case_study.title}
@@ -55,6 +58,10 @@ export default function CaseStudy() {
         <BlockRenderer key={idx} block={block} />
       ))}
       </div>
+      <Footer/>
+      </div>
+      
     </div>
+    </>
   );
 }
