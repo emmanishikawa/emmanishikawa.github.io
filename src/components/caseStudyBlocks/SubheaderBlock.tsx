7a27eb958be61846e47ec4797de2111a255ded76
emmanishikawa <emmanishikawa@gmail.com>
2025-10-07 19:25:48 -0700

    change margins

--- src/components/caseStudyBlocks/SubheaderBlock.tsx
+++ src/components/caseStudyBlocks/SubheaderBlock.tsx
@@ -3,5 +3,5 @@ interface SubheaderProps {
 }
 
 export default function ParagraphBlock({ text }: SubheaderProps) {
-  return <p className="my-2 text-[20px] font-bold text-white">{text}</p>;
+  return <h2 className="my-2 text-[20px] font-bold text-white">{text}</h2>;
 }
